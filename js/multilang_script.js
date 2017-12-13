a1deaabe6600dcda3451632d69a87fcca7ae712a
julioibarguren <jibarguren@qubit.tv>
2017-12-13 15:16:23 -0300

    lang fixes

--- js/multilang_script.js
+++ js/multilang_script.js
@@ -1,55 +1,48 @@
-//objengo el lang del body
-
-
-//Cargo el template del header cuando se carga la pagina.
-var UIController = (function(){
-
+var UIController = (function () {
     var DOMstrings = {
         navbarRight: '.tooglenav',
         langButton: '.lang-button'
     }
 
-    var getJSON = function(url, callback) {
+    var getJSON = function (url, callback) {
         var xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
         xhr.responseType = 'json';
-        xhr.onload = function() {
-          var status = xhr.status;
-          if (status === 200) {
-            callback(null, xhr.response);
-          } else {
-            callback(status, xhr.response);
-          }
+        xhr.onload = function () {
+            var status = xhr.status;
+            if (status === 200) {
+                callback(null, xhr.response);
+            } else {
+                callback(status, xhr.response);
+            }
         };
         xhr.send();
     };
 
-    function createCookie(name, value, days){
+    function createCookie(name, value, days) {
         var expires = "";
         if (days) {
             var date = new Date();
-            date.setTime(date.getTime() + (days*24*60*60*1000));
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
             expires = "; expires=" + date.toUTCString();
         }
-        console.log('creo cookie')
         document.cookie = name + "=" + value + expires + "; path=/";
         var cookiee = document.cookie = name + "=" + value + expires + "; path=/";
     }
+
     function readCookie(name) {
         var nameEQ = name + "=";
         var ca = document.cookie.split(';');
-        for(var i=0;i < ca.length;i++) {
+        for (var i = 0; i < ca.length; i++) {
             var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
         }
         return null;
     }
-    
-    function setNavLang(newlang){
-        console.log('entra')
+
+    function setNavLang(newlang) {
         var navMenu, html, init, about, services, contact, navbar, mainFlagImg, mainFlagText, flagUrl;
-        
         navbar = '<li><a class="inicioBtn" href="index.html">%home%</a></li><li><a class="aboutBtn" href="index.html#sobreNosotros">%about%</a></li><li><a class="servicesBtn" href="index.html#services">%services%</a></li><li><a class="contactBtn" href="contacto.html">%contact%</a></li>';
         navMenu = document.querySelector(DOMstrings.navbarRight);
         mainFlagImg = document.getElementById('flag_main');
@@ -57,113 +50,81 @@ var UIController = (function(){
         flagUrl = 'img/flag-' + newlang + '.jpg';
         html = navbar;
         document.getElementById('flag_main').setAttribute('src', flagUrl);
-
         document.getElementById('lang_indicator').textContent = newlang;
-
-        if(newlang === 'es'){  
-           init = 'INICIO';
-           about = 'SOBRE NOSOTROS';
-           services = 'SERVICIOS';
-           contact = 'CONTACTO';
-           
-
-        } else if(newlang === 'en'){
+        if (newlang === 'es') {
+            init = 'INICIO';
+            about = 'SOBRE NOSOTROS';
+            services = 'SERVICIOS';
+            contact = 'CONTACTO';
+        } else if (newlang === 'en') {
             init = 'HOME';
             about = 'ABOUT US';
             services = 'SERVICES';
             contact = 'CONTACT';
-           
-
-        } else if(newlang === 'pt'){
+        } else if (newlang === 'pt') {
             init = 'INÍCIO';
             about = 'ACERCA DE NÓS';
             services = 'SERVIÇOS';
             contact = 'CONTATO';
         }
-
         newHtml = html.replace('%home%', init);
         newHtml = newHtml.replace('%lang%', newlang);
         newHtml = newHtml.replace('%about%', about);
         newHtml = newHtml.replace('%services%', services);
         newHtml = newHtml.replace('%contact%', contact);
-       
         document.querySelector(DOMstrings.navbarRight).innerHTML = newHtml;
         createCookie('lang', newlang, 30);
-        
     }
-    
-    // function replaceText(data){
-    //     var translates = document.querySelectorAll('.translate');       
-    //     for(var i = 0; i < translates.length; i++){
-    //         console.log(translates[i]);
-    //         console.log(data.contact.title)
-    //         var a = translates[i].id;
-    //         var b = data + '.' + a;
-    //         console.log(b)      
-    //         document.getElementById(translates[i].id).textContent = b
-    //     }
-    // }
-    function loadTranslateJson(){
+
+    function loadTranslateJson() {
         var cookieLang = readCookie('lang')
         var jsonUrl = '../js/' + cookieLang + '.json'
-
         var currentLocation = window.location.pathname;
         var URLdomain = window.location.host;
         var currentUrl = URLdomain + currentLocation;
-      
-        getJSON(jsonUrl ,
-        function(err, data) { 
-          if (err !== null) {
-            alert('Something went wrong: ' + err);
-          } else {
-              if(cookieLang !== 'es'){
-                var translates = document.querySelectorAll('.translate');       
-                for(var i = 0; i < translates.length; i++){
-                    var a = translates[i].id;
-                    translates[i].innerHTML = data[a]
-                    
-                }
-                if(currentLocation === '/contacto.html'){
-                    var inputName = document.getElementById('contact_form_name').innerHTML;
-                    var inputEmail = document.getElementById('contact_form_email').innerHTML;
-                    var inputMessage = document.getElementById('contact_form_message').innerHTML;
-                    var inputSubmit = document.getElementById('contact_form_button').innerHTML;
-            
-                    document.getElementById('form_name').placeholder = inputName;
-                    document.getElementById('form_email').placeholder = inputEmail;
-                    document.getElementById('form_message').placeholder = inputMessage;
-                    document.getElementById('form_button').value = inputSubmit;
-                    
-            
+        getJSON(jsonUrl,
+            function (err, data) {
+                if (err !== null) {
+                    alert('Something went wrong: ' + err);
+                } else {
+                    if (cookieLang !== 'es') {
+                        var translates = document.querySelectorAll('.translate');
+                        for (var i = 0; i < translates.length; i++) {
+                            var a = translates[i].id;
+                            translates[i].innerHTML = data[a]
+                        }
+                        if (currentLocation === '/contacto.html') {
+                            var inputName = document.getElementById('contact_form_name').innerHTML;
+                            var inputEmail = document.getElementById('contact_form_email').innerHTML;
+                            var inputMessage = document.getElementById('contact_form_message').innerHTML;
+                            var inputSubmit = document.getElementById('contact_form_button').innerHTML;
+                            document.getElementById('form_name').placeholder = inputName;
+                            document.getElementById('form_email').placeholder = inputEmail;
+                            document.getElementById('form_message').placeholder = inputMessage;
+                            document.getElementById('form_button').value = inputSubmit;
+                        }
                     }
-              }
-            
-          }
-          
-        }); 
-                
+                }
+            });
     }
+
     return {
-        
-        setNewLang: function(){
+        setNewLang: function () {
             var newLang = readCookie('lang')
-            document.getElementById('btn-es').addEventListener('click', function(){ 
-                newLang = 'es' 
-                console.log(newLang)
+            document.getElementById('btn-es').addEventListener('click', function () {
+                newLang = 'es'
                 setNavLang(newLang);
                 loadTranslateJson();
                 location.reload();
             });
-            document.getElementById('btn-en').addEventListener('click', function(){ 
-                newLang = 'en' 
-                console.log(newLang)
+            document.getElementById('btn-en').addEventListener('click', function () {
+                newLang = 'en'
                 setNavLang(newLang);
                 loadTranslateJson();
                 location.reload();
             });
-            document.getElementById('btn-pt').addEventListener('click', function(){ 
-                newLang = 'pt' 
-                console.log(newLang)
+            document.getElementById('btn-pt').addEventListener('click', function () {
+                newLang = 'pt'
                 setNavLang(newLang);
                 location.reload();
             });
@@ -173,43 +134,33 @@ var UIController = (function(){
     }
 })();
 
-//asigno el lang en el body
-var translatorController = (function(){
-
-    return{
-        getUrl: function(lang){
+var translatorController = (function () {
+    return {
+        getUrl: function (lang) {
             var url = window.location.href;
-            
             var newUrl = url + '?' + lang;
-            
-            // window.location.href = newUrl;
-            console.log(newUrl);
         },
-        
-        setHtmlLang: function(lang){
-            document.getElementsByTagName('html')[0].setAttribute('lang',lang);
+        setHtmlLang: function (lang) {
+            document.getElementsByTagName('html')[0].setAttribute('lang', lang);
         }
-      
+
     }
 })();
 
-var siteController = (function(translatorCtrl, UICtrl){
-    
-    var changeLang = function(){
-       UICtrl.setNewLang();
-       
+var siteController = (function (translatorCtrl, UICtrl) {
+    var changeLang = function () {
+        UICtrl.setNewLang();
     };
+    
     return {
-        init: function() {
+        init: function () {
             console.log('aplication started');
-            changeLang();   
+            changeLang();
         }
     };
-    
 })(translatorController, UIController);
 
 siteController.init();
-//traigo el json con los textos de la pagina segun el lang del body
 
 
 
